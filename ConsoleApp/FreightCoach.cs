02b4a72d430a8d09ad0bdc1b3aab7bcb3701e8ae
Mahmudxoja <52691444+Mahmudxoja@users.noreply.github.com>
2019-07-09 11:48:16 +0500

    Add files via upload

---
+++ ConsoleApp/FreightCoach.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    static class FreightCoach
+    {
+        /// <summary>
+        /// Checks the correct number of freight coach.
+        /// Freight coach number consists of 8 digits.
+        /// </summary>
+        /// <param name="coach_number">The number of freight coach</param>
+        /// <returns></returns>
+        public static bool IsDigit(string coach_number)
+        {
+            if (coach_number.Length != 8)
+                return false;
+
+            for (int i = 0; i < 8; i++)
+                if (!char.IsDigit(coach_number[i]))
+                    return false;
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// Check the number of the freight coach.
+        /// </summary>
+        /// <param name="coach_number">The number of freight coach</param>
+        /// <param name="true_number">For the correct check digit</param>
+        /// <returns></returns>
+        public static bool IsFreightCoach(string coach_number, out int true_number)
+        {
+            int first_7_number = Convert.ToInt32(coach_number.Substring(0, 7));
+            int last_number = Convert.ToInt32(coach_number[7].ToString());
+
+            int sum = 0;
+            int step = 0;
+            while (first_7_number > 0)
+            {
+                int temp = 0;
+                if (step % 2 == 0)
+                    temp = (first_7_number % 10) * 2;
+                else
+                    temp = first_7_number % 10;
+
+                while (temp >= 10)
+                {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+
+                sum += temp;
+                first_7_number /= 10;
+
+                step++;
+            }
+
+
+            // If sum % 10 is 0, then last number is 0
+            if (sum % 10 == 0)
+            {
+                true_number = 0;
+                return 0 == last_number;
+            }
+
+            // If last number is 0, then sum % 10 is 0
+            if (last_number == 0)
+            {
+                true_number = 10 - (sum % 10);
+                return 10 - (sum % 10) == 0;
+            }
+
+
+            true_number = 10 - (sum % 10);
+            return true_number == last_number;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coach_number = "";
+
+            do
+            {
+                Console.WriteLine("Please, enter 8 number:");
+                coach_number = Console.ReadLine();
+                Console.Clear();
+            } while (!FreightCoach.IsDigit(coach_number));
+            Console.WriteLine("Freight coach number {0}", coach_number);
+
+
+            if(FreightCoach.IsFreightCoach(coach_number, out int true_number))
+            {
+                Console.WriteLine("Number of freight coach is correctly!");
+            }
+            else
+            {
+                Console.WriteLine("Number of freight coach is wrong!");
+                Console.WriteLine("Last number is {0}", true_number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
